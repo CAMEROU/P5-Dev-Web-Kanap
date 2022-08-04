2f4360778387782d2cb46942d6c1ee9a22d2f450
CAMEROU <98232630+CAMEROU@users.noreply.github.com>
2022-08-04 13:39:46 +0800

    get data

--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,14 @@
 fetch("http://localhost:3000/api/products")
-.then((res) =>res.json())
-.then((data) => addproducts(data))
-   
- 
- function addproducts(data) {
- const imageUrl = data [0].imageUrl
- 
- const anchor = document.createElement("a")
- anchor.href = imageUrl
- anchor.text =  "Kanap Sinopé"
- const items = document.querySelector("#items")
- items.appendChild(anchor)
- 
-    
- }
+.then((res)  => res.json())
+.then((data) => addproduct(data))
 
+function addproduct(data){
+const imageUrl =  data[0].imageUrl 
+  
+  const anchor = document.createElement("a")
+  anchor.href= imageUrl
+  anchor.text = "Kanap Sinopé"
+  
+  const items = document.querySelector("#items")
+  items.appendChild(anchor)
+}
